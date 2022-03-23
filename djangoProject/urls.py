1cbece17b824ce731fcd35e2e1f4c0b2d7cc945c
kayshad <kaisslama@gmail.com>
2022-03-23 08:16:08 +0100

    Add files via upload

---
+++ djangoProject/urls.py
@@ -0,0 +1,44 @@
+"""djangoProject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app.views import (
+    DefaultFormByFieldView,
+    DefaultFormsetView,
+    DefaultFormView,
+    FormHorizontalView,
+    FormInlineView,
+    FormWithFilesView,
+    HomePageView,
+    MiscView,
+    PaginationView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", HomePageView.as_view(), name="home"),
+    path("home", HomePageView.as_view(), name="home"),
+    path("formset", DefaultFormsetView.as_view(), name="formset_default"),
+    path("form", DefaultFormView.as_view(), name="form_default"),
+    path("form_by_field", DefaultFormByFieldView.as_view(), name="form_by_field"),
+    path("form_horizontal", FormHorizontalView.as_view(), name="form_horizontal"),
+    path("form_inline", FormInlineView.as_view(), name="form_inline"),
+    path("form_with_files", FormWithFilesView.as_view(), name="form_with_files"),
+    path("pagination", PaginationView.as_view(), name="pagination"),
+    path("misc", MiscView.as_view(), name="misc"),
+
+]
+
